775fab9ff8c64e046b6c70bd4440a2fb02e9eceb
Jorge Martinez <george16180@live.com>
2023-09-28 22:06:00 -0500

    adding validations for AddCountry method in CountryService

--- Services/CountryService.cs
+++ Services/CountryService.cs
@@ -13,6 +13,23 @@ namespace Services
         }
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
+            
+            //Validation: countryAddRequest can't be null
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+            //Validation: CountryName can't be null
+            if(countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+            //Validation: CountryName can't be duplicate
+            if(_countries.Where(country => country.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
             //Convert object from CountryAddRequest to Country
             Country country = countryAddRequest.ToCountry();           
 
