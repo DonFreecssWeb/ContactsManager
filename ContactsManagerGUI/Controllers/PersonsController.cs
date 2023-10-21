81039b4a576048815b831875cfa4732aa46672be
Jorge Martinez <george16180@live.com>
2023-10-20 20:24:18 -0500

    changing link of font awesome in _Layout Created table of persons, combobox of searchBy, input, button and hyperlinks in thead to person name. Adding code to search by all fields and sort the list ascending or descending based of parametres from View

--- ContactsManagerGUI/Controllers/PersonsController.cs
+++ ContactsManagerGUI/Controllers/PersonsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServiceContracts;
 using ServiceContracts.DTO;
+using ServiceContracts.Enums;
 
 namespace ContactsManagerGUI.Controllers
 {
@@ -14,12 +15,33 @@ namespace ContactsManagerGUI.Controllers
         }
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
-        {
-            List<PersonResponse> person_list = _personService.GetAllPersons();
+        public IActionResult Index(string searchBy, string? searchString,
+            string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.Ascending)
+        {           
+           //Search
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                {nameof(PersonResponse.PersonName), "Person name" },
+                {nameof(PersonResponse.Email),"Email" },
+                {nameof(PersonResponse.Age),"Age" },
+                {nameof(PersonResponse.Address),"Address" },
+                {nameof(PersonResponse.Gender), "Gender" },
+                {nameof(PersonResponse.Country),"Country" },
+                {nameof(PersonResponse.DateOfBirth),"Date of birth" },
+                {nameof(PersonResponse.ReceiveNewsLetters),"Receive News Letters" }
+            };
+           //if searchString is null or empty, it return all persons 
+           List<PersonResponse> person_list = _personService.GetFilteredPersons(searchBy, searchString);
 
+            //Persistence of values fields
+            ViewBag.currentSearchBy = searchBy; 
+            ViewBag.currentSearchString = searchString;
 
-            return View(person_list);
+            //Sort
+            List<PersonResponse> sortedPersons = _personService.GetSortedPersons(person_list, sortBy, sortOrder);
+            ViewBag.currentSortBy = sortBy;
+            ViewBag.currentSortOrder = sortOrder;
+            return View(sortedPersons);
         }
     }
 }
